3ab38048d67a6f89a211687d97b3df1a64445fc2
ARIBILGI <ARIBILGI@DESKTOP-0JD0258>
2022-08-06 17:06:42 +0300

    Add project files.

---
+++ 10_Odev_Lamba/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _10_Odev_Lamba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool lamb1 = true, lamb2 = true, lamb3 = true;
+            if (lamb1)
+            {
+                if (lamb2 && lamb3)
+                {
+                    Console.WriteLine("tüm lambalar açık");
+                }
+                else if (!lamb2 && !lamb3)
+                {
+                    Console.WriteLine("yalnız bir lamba açık");
+                }
+                else
+                {
+                    Console.WriteLine("2 lamba açık");
+                }
+            }
+            else if (lamb2)
+            {
+                if (lamb1 && lamb3)
+                {
+                    Console.WriteLine("tüm lambalar açık");
+                }
+                else if (!lamb1 && !lamb3)
+                {
+                    Console.WriteLine("yalnız bir lamba acık");
+                }
+                else
+                {
+                    Console.WriteLine("2 lamba açık");
+                }
+            }
+            else if (lamb3)
+            {
+                if (lamb1 && lamb2)
+                {
+                    Console.WriteLine("tüm lambalar açık");
+                }
+                else if (!lamb1 && !lamb2)
+                {
+                    Console.WriteLine("yalnız bir lamba acık");
+                }
+                else
+                {
+                    Console.WriteLine("2 lamba açık");
+                }
+            }
+            else
+            {
+                Console.WriteLine("tüm lambalar kapalı");
+            }
+        }
+    }
+}
---
+++ 20_Odev_AsalSayıToplam/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _20_Odev_AsalSayıToplam
+{
+    internal class Program
+    {
+        static bool AsalMı(int sayi)
+        {
+            bool sonuc = true;
+            for (int i = 2; i < sayi - 1; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    sonuc = false;
+                    break;
+                }
+            }
+            return sonuc;
+        }
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            for (int i = 2; i <= 1000; i++)
+            {
+                if (AsalMı(i))
+                {
+                    toplam += i;
+                }
+            }
+            Console.WriteLine(toplam);
+        }
+}
